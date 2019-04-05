9b40faa58b3919b1e0b3f93aef3d16c3bf01dac7
Chris Angelico <rosuav@gmail.com>
2019-04-06 09:45:28 +1100

    Toy

--- database.py
+++ database.py
@@ -218,6 +218,7 @@ def get_public_timer_details(id):
 		cur.execute("select twitchid, title, delta, maxtime, styling from mustard.timers where id=%s", (id,))
 		info = cur.fetchone()
 		if not info: return None
+		print(info)
 		twitchid = info.pop("twitchid")
 		cur.execute("select sched_timezone, schedule from mustard.users where twitchid=%s", (twitchid,))
 		sched = cur.fetchone()
